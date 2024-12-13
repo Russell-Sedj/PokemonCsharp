b718746175bfdd7aaa09f2ff5457009c397fa3da
Russell-Sedj <34935151+Russell-Sedj@users.noreply.github.com>
2024-12-13 09:28:55 +0100

    Ajoutez des fichiers projet.

---
+++ Attaque.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Attaque
+    {
+        private  string nom;
+        private string type;
+        private string categorieAttaque;
+        private int precision;
+        private int puissance;
+        private int pp;
+
+        public string Nom { get => nom; }
+        public int Pp { get => pp; set => pp = value; }
+
+        public Attaque(string nom, string type, string categorieAttaque, int precision, int puissance, int pp)
+        {
+            this.nom = nom;
+            this.type = type;
+            this.categorieAttaque = categorieAttaque;
+            this.precision = precision;
+            this.puissance = puissance;
+            this.pp = pp;
+        }
+
+        public int CalculerDegats (Pokemon attaquant, Pokemon defensseur)
+        {
+            //pvPerdus = ((((niv * 0.4 + 2) * att * pui) / (def * 50)) + 2) * (CM = stab * precision)
+            int degats = 0;
+            double STAB = 1;
+            if (attaquant.Types.Contains(this.type))
+            {
+                STAB = 1.5;
+            }
+
+            if (this.categorieAttaque.Equals("physique"))
+            {
+                degats = (int)(((((attaquant.Niveau * 0.4 + 2) * attaquant.Attaque * this.puissance) / (defensseur.Defense * 50)) + 2) * (STAB * (this.precision / 100)));
+            }
+            else
+            {
+                degats = (int)(((((attaquant.Niveau * 0.4 + 2) * attaquant.AttaqueSpeciale * this.puissance) / (defensseur.DefenseSpeciale * 50)) + 2) * (STAB * (this.precision / 100)));
+            }
+
+            return degats;
+        }
+
+        public void Afficher ()
+        {
+            Console.WriteLine("Nom : " + this.nom);
+            Console.WriteLine("Type : " + this.type);
+            Console.WriteLine("Categorie : " + this.categorieAttaque);
+            Console.WriteLine("Precision : " + this.precision);
+            Console.WriteLine("Puissance : " + this.puissance);
+            Console.WriteLine("PP : " + this.pp);
+        }
+    }
+}
---
+++ Pokemon.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Pokemon
+    {
+        private string nom;
+        private int prix;
+        private List<string> types;
+        private int pointsDeVie;
+        private int niveau;
+        private int attaque;
+        private int attaqueSpeciale;
+        private int defense;
+        private int defenseSpeciale;
+        private int vitesse;
+        private List<Attaque> attaques;
+
+        public string Nom { get => nom; }
+        public int Prix { get => prix; }
+        public List<string> Types { get => types; }
+        public int PointsDeVie { get => pointsDeVie; set => pointsDeVie = value; }
+        public int Niveau { get => niveau; }
+        public int Attaque { get => attaque; }
+        public int AttaqueSpeciale { get => attaqueSpeciale; }
+        public int Defense { get => defense; }
+        public int DefenseSpeciale { get => defenseSpeciale; }
+        public int Vitesse { get => vitesse; }
+
+        public Pokemon(string nom, int prix, List<string> types, int pointsDeVie, int niveau, int attaque, int attaqueSpeciale, int defense, int defenseSpeciale, int vitesse, List<Attaque> attaques)
+        {
+            this.nom = nom;
+            this.prix = prix;
+            this.types = types;
+            this.pointsDeVie = pointsDeVie;
+            this.niveau = niveau;
+            this.attaque = attaque;
+            this.attaqueSpeciale = attaqueSpeciale;
+            this.defense = defense;
+            this.defenseSpeciale = defenseSpeciale;
+            this.vitesse = vitesse;
+            this.attaques = attaques;
+        }
+
+        public void AjouterAttaque(Attaque attaque)
+        {
+            this.attaques.Add(attaque);
+        }
+
+        public void Attaquer(Pokemon enemi, Attaque attaqueUtilisee)
+        {
+            int degats = attaqueUtilisee.CalculerDegats(this, enemi);
+            enemi.PointsDeVie -= degats;
+            Console.WriteLine($"{this.nom} attaque {enemi.Nom} avec {attaqueUtilisee.Nom} et lui inflige {degats} points de dégats.");
+
+            attaqueUtilisee.Pp -= 1;
+        }
+
+        public bool EstKO()
+        {
+            return this.pointsDeVie <= 0;
+        }
+
+        public void AfficherAttaques()
+        {
+            Console.WriteLine("Attaques de " + this.nom + " :");
+            foreach (Attaque attaque in this.attaques)
+            {
+                Console.WriteLine("-----------");
+                attaque.Afficher();
+                Console.WriteLine("-----------");
+            }
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("Infos Pokemon : ");
+            Console.WriteLine("Nom : " + this.nom);
+            Console.WriteLine("Prix : " + this.prix);
+            Console.WriteLine("Types : ");
+            foreach (string type in this.types)
+            {
+                Console.WriteLine(type);
+            }
+            Console.WriteLine("Points de vie : " + this.pointsDeVie);
+            Console.WriteLine("Niveau : " + this.niveau);
+            Console.WriteLine("Attaque : " + this.attaque);
+            Console.WriteLine("Attaque speciale : " + this.attaqueSpeciale);
+            Console.WriteLine("Defense : " + this.defense);
+            Console.WriteLine("Defense speciale : " + this.defenseSpeciale);
+            Console.WriteLine("Vitesse : " + this.vitesse);
+        }
+    }
+}
