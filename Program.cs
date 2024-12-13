142dffb73ebf5c342ca42aebbd6c63659c422821
Russell-Sedj <34935151+Russell-Sedj@users.noreply.github.com>
2024-12-13 15:43:32 +0100

    Refactorisation du code de test dans `Program.cs`
    Le code de test de la classe `Pokemon` a été supprimé. Ce code comprenait la création d'un Pokémon "Pikachu" avec des attaques spécifiques et l'affichage de ces attaques. À la place, une nouvelle instance de la classe `Jeu` est maintenant créée dans la méthode `Main`.

--- Program.cs
+++ Program.cs
@@ -4,15 +4,7 @@
     {
         static void Main(string[] args)
         {
-            //Tester classe pokemon, creer un pokemon ajoute attaque, afficher liste attaques
-            Pokemon p = new Pokemon("Pikachu", 100, new List<string> { "electrique" }, 100, 50, 50, 50, 50, 50, 50, new List<Attaque>());
-            Attaque a = new Attaque("eclair", "electrique", "speciale", 100, 100, 10);
-            Attaque b = new Attaque("danse lames", "normal", "physique", 100, 100, 10);
-            Attaque c = new Attaque("lance flamme", "feu", "speciale", 100, 100, 10);
-            p.AjouterAttaque(a);
-            p.AjouterAttaque(b);
-            p.AjouterAttaque(c);
-            p.Afficher();
+            Jeu jeu = new Jeu();
         }
     }
 }
