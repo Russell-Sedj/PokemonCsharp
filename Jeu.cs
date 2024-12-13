6a5dc1c66467e981fc9edfb00e7555e4e042edb0
Russell-Sedj <34935151+Russell-Sedj@users.noreply.github.com>
2024-12-13 11:44:17 +0100

    Ajout de la classe Jeu avec gestion des joueurs et rounds
    - Ajout de la liste `joueurs` et de l'entier `nombreRounds` dans la classe `Jeu`.
    - Ajout d'un constructeur pour initialiser `joueurs` et définir `nombreRounds` à 3.
    - Ajout de la méthode `Jouer` pour gérer l'inscription des joueurs et la sélection de leurs pokémons.

--- Jeu.cs
+++ Jeu.cs
@@ -8,5 +8,29 @@ namespace ConsoleApp1
 {
     internal class Jeu
     {
+        private List<Joueur> joueurs;
+        private int nombreRounds;
+
+        public Jeu()
+        {
+            this.joueurs = new List<Joueur>();
+            this.nombreRounds = 3;
+        }
+
+        public void Jouer()
+        {
+            Console.WriteLine("Entrez le nom du joueur :");
+            string nom = Console.ReadLine();
+            joueurs.Add(new Joueur(nom));
+            Console.WriteLine("Choisissez vos pokemons :");
+            joueurs[0].ChoisirPokemon();
+            Console.WriteLine("Entrez le nom du joueur 2 :");
+            nom = Console.ReadLine();
+            joueurs.Add(new Joueur(nom));
+            Console.WriteLine("Choisissez vos pokemons :");
+            joueurs[1].ChoisirPokemon();
+
+
+        }
     }
 }
